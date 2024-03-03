4cfd2e1cbf35cb9652e055f317ad9e5bd9c3b792
Adonis <adn221294@gmail.com>
2024-03-03 19:18:15 -0400

    Basic estructure and functionalities completed

---
+++ addAndRemoveTrianglesModule.js
@@ -0,0 +1,65 @@
+(function addAndRemoveTrianglesModule() {
+	var addTriangleBtn = document.getElementById("add-triangle-btn");
+	var trianglesTableBody = document.getElementById("triangles-table-body");
+	var rowId = 1;
+
+	addTriangleBtn.addEventListener("click", () => {
+		addRowTotable();
+	});
+
+	function addRowTotable() {
+		let a = parseValue(document.getElementById("a").value);
+		let b = parseValue(document.getElementById("b").value);
+		let c = parseValue(document.getElementById("c").value);
+
+		trianglesTableBody.innerHTML += `
+			<tr id=row-${rowId}>
+				<td>
+					${rowId}
+				</td>
+				<td>
+					${a}
+				</td>
+				<td>
+					${b}
+				</td>
+				<td>
+					${c}
+				</td>
+				<td>
+					${calcSemiperimeter(a, b, c)}
+				</td>
+				<td>
+					<button id=row-${rowId} class="quitBtns">
+						Borrar
+					</button>
+				</td>
+			</tr>
+		`;
+
+		//Deberia haber una forma mas eficiente de hacer esto:
+		for (let btn of document.getElementsByClassName("quitBtns")) {
+			btn.onclick = () => {
+				//console.log(btn.id);
+				removeRow(btn.id);
+			};
+		}
+		rowId += 1;
+	}
+
+	function parseValue(val) {
+		if (!val) {
+			return 0;
+		}
+		return Number(val);
+	}
+
+	function calcSemiperimeter(a, b, c) {
+		return (a + b + c) / 2;
+	}
+
+	function removeRow(btnId) {
+		let row = document.getElementById(btnId);
+		row.remove();
+	}
+})();
---
+++ computeAreaModule.js
@@ -0,0 +1,40 @@
+(function computeAreaModule() {
+	var areaLabel = document.getElementById("total-area");
+	var trianglesTableBody = document.getElementById("triangles-table-body");
+
+	var tableChangeObserver = new MutationObserver(onTableChange);
+
+	const config = { attributes: true, childList: true, subtree: true };
+	tableChangeObserver.observe(trianglesTableBody, config);
+
+	function onTableChange() {
+		console.log("Recalculando Areas...");
+
+		let newArea = 0;
+		let currentCells;
+		let currentA;
+		let currentB;
+		let currentC;
+		let currentS;
+
+		for (let row of trianglesTableBody.rows) {
+			currentCells = row.cells;
+			currentA = Number(currentCells[1].innerHTML);
+			currentB = Number(currentCells[2].innerHTML);
+			currentC = Number(currentCells[3].innerHTML);
+			currentS = Number(currentCells[4].innerHTML);
+
+			console.log(currentA, currentB, currentC, currentS);
+
+			newArea += formulaHeron(currentA, currentB, currentC, currentS);
+		}
+
+		areaLabel.innerHTML = `${newArea.toFixed(3)} u^2`;
+	}
+
+	function formulaHeron(a, b, c, s) {
+		//var s = (a + b + c) / 2;
+		let area = Math.pow(s * (s - a) * (s - b) * (s - c), 1 / 2);
+		return area;
+	}
+})();
