cc29adc8fb419a851c74740eb57d23af5592370e
Adonis Nunez <adn221294@gmail.com>
2024-03-04 09:47:43 -0400

    Mejoras en el layout de la tabla de triangulos y se agrego una funcion para validar triangulos

--- addAndRemoveTrianglesModule.js
+++ addAndRemoveTrianglesModule.js
@@ -1,18 +1,19 @@
 (function addAndRemoveTrianglesModule() {
-	var addTriangleBtn = document.getElementById("add-triangle-btn");
-	var trianglesTableBody = document.getElementById("triangles-table-body");
-	var rowId = 1;
+  var addTriangleBtn = document.getElementById("add-triangle-btn");
+  var trianglesTableBody = document.getElementById("triangles-table-body");
+  var rowId = 1;
 
-	addTriangleBtn.addEventListener("click", () => {
-		addRowTotable();
-	});
+  addTriangleBtn.addEventListener("click", () => {
+    addRowTotable();
+  });
 
-	function addRowTotable() {
-		let a = parseValue(document.getElementById("a").value);
-		let b = parseValue(document.getElementById("b").value);
-		let c = parseValue(document.getElementById("c").value);
+  function addRowTotable() {
+    let a = parseValue(document.getElementById("a").value);
+    let b = parseValue(document.getElementById("b").value);
+    let c = parseValue(document.getElementById("c").value);
 
-		trianglesTableBody.innerHTML += `
+    if (isValidTriangle(a, b, c)) {
+      trianglesTableBody.innerHTML += `
 			<tr id=row-${rowId}>
 				<td>
 					${rowId}
@@ -36,30 +37,39 @@
 				</td>
 			</tr>
 		`;
+      //Deberia haber una forma mas eficiente de hacer esto:
+      for (let btn of document.getElementsByClassName("quitBtns")) {
+        btn.onclick = () => {
+          removeRow(btn.id);
+        };
+      }
+      rowId += 1;
+    } else {
+      alert(
+		//Esto se podria mejorar usando elementos del DOM para crear el mensaje.
+        "Estas longitudes no cumplen con la desigualdad triangular, por lo que no es posible formar un triángulo con estas."
+      );
+    }
+  }
 
-		//Deberia haber una forma mas eficiente de hacer esto:
-		for (let btn of document.getElementsByClassName("quitBtns")) {
-			btn.onclick = () => {
-				//console.log(btn.id);
-				removeRow(btn.id);
-			};
-		}
-		rowId += 1;
-	}
+  function parseValue(val) {
+    if (!val) {
+      return 0;
+    }
+    return Number(val);
+  }
 
-	function parseValue(val) {
-		if (!val) {
-			return 0;
-		}
-		return Number(val);
-	}
+  function calcSemiperimeter(a, b, c) {
+    return (a + b + c) / 2;
+  }
 
-	function calcSemiperimeter(a, b, c) {
-		return (a + b + c) / 2;
-	}
+  function removeRow(btnId) {
+    let row = document.getElementById(btnId);
+    row.remove();
+  }
 
-	function removeRow(btnId) {
-		let row = document.getElementById(btnId);
-		row.remove();
-	}
+  //Ver el teorema de la desigualdad triangular
+  function isValidTriangle(a, b, c) {
+    return a < b + c && b < a + c && c < a + b;
+  }
 })();
